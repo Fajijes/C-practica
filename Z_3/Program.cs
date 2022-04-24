17426f4db04f13a9b48750c84cae198785dfd0b1
Nikita <parnikaao@mail.ru>
2022-04-24 15:56:23 +0700

    Seminar_1

---
+++ Z_3/Program.cs
@@ -0,0 +1,6 @@
+﻿string s_a = Console.ReadLine(); 
+int a = int.Parse(s_a);
+int x;
+x = a * a + a + 2;
+
+Console.WriteLine(x);
---
+++ Z_4/Program.cs
@@ -0,0 +1,10 @@
+﻿string s = Console.ReadLine();
+int x = int.Parse(s);
+int y = -x;
+
+while (y <= x)
+{
+    Console.Write($"{y} ");
+    y = y + 1;
+}
+
